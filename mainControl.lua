2846ddf0648fefefd5a13113ecbeca0a273c9e86
Sean Meyer <seanm0914@gmail.com>
2023-04-30 20:13:07 -0400

    add player tracking

--- mainControl.lua
+++ mainControl.lua
@@ -2,9 +2,12 @@ rednet.open("left")
 local monitor = peripheral.wrap("right")
 while true do
     local id, message = rednet.receive()
+    local id2, message2 = rednet.receive()
     monitor.clear()
     monitor.setCursorPos(1,1)
     monitor.setTextColor(colors.cyan)
     print(message)
+    monitor.write(message)
+    monitor.setCursorPos(5,1)
     monitor.write(message)
 end
